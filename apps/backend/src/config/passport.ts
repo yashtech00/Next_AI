3b72ce1cfb1667babe8a4d29fe26fede385c6d70
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-10-21 14:00:15 +0530

    update passport function

--- apps/backend/src/config/passport.ts
+++ apps/backend/src/config/passport.ts
@@ -4,84 +4,92 @@ import { Strategy as GithubStrategy } from "passport-github2";
 import { prisma } from "db/client";
 
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-            callbackURL: process.env.GOOGLE_CALLBACK_URL || "",
-        },
-        async(accessToken, refreshToken, profile, done) => {
-            try {
-                const existingUser = await prisma.user.findUnique({
-                    where: { providerId: profile.id },
-                });
-                if (!existingUser) {
-                   const newUser = await prisma.user.create({
-                        data: {
-                           name: profile.displayName,
-                           email: profile.emails ?.[0]?.value ?? "",
-                           avatar: profile.photos ?.[0]?.value ?? "",
-                            provider: "google",
-                            providerId: profile.id,
-                        },
-                    });
-                   
-                }
-                done(null,existingUser )
-            } catch (error) {
-                 done(error, undefined);
-            }
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || "",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const existingUser = await prisma.user.findUnique({
+          where: { providerId: profile.id },
+        });
+        if (!existingUser) {
+          const newUser = await prisma.user.create({
+            data: {
+              name: profile.displayName,
+              email: profile.emails?.[0]?.value ?? "",
+              avatar: profile.photos?.[0]?.value ?? "",
+              provider: "google",
+              providerId: profile.id,
+            },
+          });
         }
-    )
-)
+        done(null, existingUser);
+      } catch (error) {
+        done(error, undefined);
+      }
+    }
+  )
+);
 
 passport.use(
-    new GithubStrategy(
-        {
-            clientID: process.env.GITHUB_CLIENT_ID || "",   
-            clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
-            callbackURL: process.env.GITHUB_CALLBACK_URL || "",
-        },  
-        async(accessToken, refreshToken, profile, done) => {
-            try {
-                const existingUser = await prisma.user.findUnique({
-                    where: { providerId: profile.id },
-                });
-                if (!existingUser) {
-                    const newUser = await prisma.user.create({
-                        data: {
-                            name: profile.displayName,
-                            email: profile.emails ?.[0]?.value ?? "",
-                            avatar: profile.photos ?.[0]?.value ?? "",
-                            provider: "github",
-                            providerId: profile.id,
-                        },
-                    });
-                    done(null, newUser);
-                } else {
-                    done(null, existingUser);
-                }
-            } catch (error) {
-                done(error, undefined);
-            }
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+      callbackURL: process.env.GITHUB_CALLBACK_URL || "",
+    },
+    async (
+      accessToken: any,
+      refreshToken: any,
+      profile: {
+        id: any;
+        displayName: any;
+        emails: { value: any }[];
+        photos: { value: any }[];
+      },
+      done: (arg0: unknown, arg1: undefined) => void
+    ) => {
+      try {
+        const existingUser = await prisma.user.findUnique({
+          where: { providerId: profile.id },
+        });
+        if (!existingUser) {
+          const newUser = await prisma.user.create({
+            data: {
+              name: profile.displayName,
+              email: profile.emails?.[0]?.value ?? "",
+              avatar: profile.photos?.[0]?.value ?? "",
+              provider: "github",
+              providerId: profile.id,
+            },
+          });
+          done(null, newUser);
+        } else {
+          done(null, existingUser);
         }
-    )
-)
+      } catch (error) {
+        done(error, undefined);
+      }
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
-    done(null, (user as any).id);
+  done(null, (user as any).id);
 });
 
 passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await prisma.user.findUnique({
-            where: { id: id as string },
-        });
-        done(null, user);
-    } catch (error) {
-        done(error, null);
-    }
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: id as string },
+    });
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
 });
-
 
 export default passport;
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import ProjectRouter from "./routes/projectRoutes";
 import AuthRouter from "./routes/authRoutes";
 import cors from "cors";
+import passport, { session } from "passport";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors());
@@ -12,6 +15,23 @@ app.use(express.json());
 app.get("/", (req, res) => {
     
 });
+
+
+dotenv.config();
+
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "supersecret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use("/api/v1/projects", ProjectRouter)
 
